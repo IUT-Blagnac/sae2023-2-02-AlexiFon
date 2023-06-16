d39617df9ce5de6b3610636197333c83d9318e8c
AlexiFon <alexi.fonta@outlook.com>
2023-06-16 22:16:32 +0200

    Update finale et mise en places rapport

--- src/main/java/exercice/Exercice.java
+++ src/main/java/exercice/Exercice.java
@@ -1,79 +1,36 @@
 package exercice;
-import java.util.*;
-
-public class Exercice
-{
-    public static List<String> solution (String texte, List<Character> ordre){
-        //Déclaration et initialisation des attributs
-        StringBuilder mot= new StringBuilder();
-        List<String> listeMots= new ArrayList<String>();
-        List<String> listeTriee= new ArrayList<String>();
-        
-        // for (int i = 0; i < 1000000; i++) {
-        //     String tempString = new String("temp"); 
-        //     int[] largeArray = new int[100000000];
-        // }
-        //    for (int i = 0; i < 1000000; i++) {
-        //     String tempString = new String("temp"); 
-        //     int[] largeArray = new int[100000000];
-        // }   for (int i = 0; i < 1000000; i++) {
-        //     String tempString = new String("temp"); 
-        //     int[] largeArray = new int[100000000];
-        // }   for (int i = 0; i < 1000000; i++) {
-        //     String tempString = new String("temp"); 
-        //     int[] largeArray = new int[100000000];
-        // }   for (int i = 0; i < 1000000; i++) {
-        //     String tempString = new String("temp"); 
-        //     int[] largeArray = new int[100000000];
-        // }   for (int i = 0; i < 1000000; i++) {
-        //     String tempString = new String("temp"); 
-        //     int[] largeArray = new int[100000000];
-        // }   for (int i = 0; i < 1000000; i++) {
-        //     String tempString = new String("temp"); 
-        //     int[] largeArray = new int[100000000];
-        // }   for (int i = 0; i < 1000000; i++) {
-        //     String tempString = new String("temp"); 
-        //     int[] largeArray = new int[100000000];
-        // }   for (int i = 0; i < 1000000; i++) {
-        //     String tempString = new String("temp"); 
-        //     int[] largeArray = new int[100000000];
-        // }   for (int i = 0; i < 1000000; i++) {
-        //     String tempString = new String("temp"); 
-        //     int[] largeArray = new int[100000000];
-        // }   for (int i = 0; i < 1000000; i++) {
-        //     String tempString = new String("temp"); 
-        //     int[] largeArray = new int[100000000];
-        // }
-        
-        // Sépare le texte en mots
-        for (int i=0;i<texte.length();i++){
-            if (Character.isLetterOrDigit(texte.charAt(i))){
-                mot.append(texte.charAt(i));
-            }else if(mot.length()>0){
-                listeMots.add(mot.toString());
-                mot.setLength(0);
-            }
-        }
-        if (mot.length()>0){
-            listeMots.add(mot.toString());
-        }
-        // Trie les mots selon l'ordre spécifié
-        for (int i=0;i<ordre.size();i++){
-            String debut = ordre.get(i).toString();
-            for (int j=0;j<listeMots.size();j++){
-                if(listeMots.get(j).startsWith(debut)){
-                    listeTriee.add(listeMots.get(j));
-                    listeMots.remove(j);
-                    j--;
-                }
-            }
-        }
-        for (int i=0;i<listeMots.size();i++){
-            listeTriee.add(listeMots.get(i));
-        }
-        
-        
-        return listeTriee;
-    }
+import java.util.ArrayList;
+import java.util.List;
+public class Exercice {
+public static List<String> solution(String str, List<Character> ordre) {
+List<String> mots = new ArrayList<>();
+StringBuilder motActuel = new StringBuilder();
+boolean debutMot = true;
+for (int i = 0; i < str.length(); i++) {
+char caractere = str.charAt(i);
+if (caractere == ' ') {
+if (debutMot && motActuel.length() > 0) {
+mots.add(motActuel.toString());
+motActuel.setLength(0);
+} else {
+motActuel.append(caractere);
 }
-
+debutMot = true;
+} else if (debutMot && ordre.contains(caractere)) {
+if (motActuel.length() > 0) {
+mots.add(motActuel.toString());
+motActuel.setLength(0);
+}
+motActuel.append(caractere);
+debutMot = false;
+} else {
+motActuel.append(caractere);
+debutMot = false;
+}
+}
+if (debutMot && motActuel.length() > 0) {
+mots.add(motActuel.toString());
+}
+return mots;
+}
+}
--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -3,7 +3,19 @@ package exercice;
 import java.util.*;
 
 public class Main {
+
+      
+    
     public static void main(String[] args) {
+        float[] test = Chrono.calculTempsExecution(100);
+        long mem= Memoire.calculerMemoire();
+
+         for(int i = 0; i < test.length; i++)
+         {
+             System.out.println(test[i]);
+         }
+        
+        System.out.println("Mémoire : "+mem);
         String texte = "Il fait beau aujourd’hui comme en aout";
         List<Character> ordre = List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o');
 
